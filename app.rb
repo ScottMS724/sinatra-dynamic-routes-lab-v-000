822ef1dba8a4cb43725c9cac01c0da4b015ff8b4
Scott Silverman <scottms724@gmail.com>
2019-06-12 17:16:23 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -31,7 +31,18 @@ class App < Sinatra::Base
     @operation = params[:operation]
     @number1 = params[:number1]
     @number2 = params[:number2]
-    
+    case params[:operation]
+    when "add"
+      "#{@number1 + @number2}"
+    when "subtract"
+      "#{@number1 - @number2}"
+    when "multiply"
+      "#{@number1 * @number2}"
+    when "divide"
+      "#{@number1 / @number2}"
+    else
+      "Invalid operation"
+    end
   end 
 
 end
