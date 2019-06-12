75cd15439fa4d3b526ac5a503f4820b25bb1d157
Scott Silverman <scottms724@gmail.com>
2019-06-12 16:54:10 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do 
     @number = params[:number]
     @phrase = params[:phrase]
-    
+    when @number = 
   end 
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do 
